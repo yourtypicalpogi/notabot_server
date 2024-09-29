fce78afcad6f41bd576d5f7a19e47351f396fc5c
yourtypicalpogi <blvpogi@gmail.com>
2024-09-29 16:37:52 +0800

    production to heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const helmet = require("helmet");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 80; // Use PORT from .env or default to 3000
+const port = 80;
 
 app.use(helmet()); // Use helmet to secure your app
 app.use(cors()); // Enable CORS
